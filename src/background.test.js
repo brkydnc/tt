28850fa059db0eab02b051d16705c1cca95992be
Berkay Dinç <berkayydinc@gmail.com>
2024-06-29 15:52:23 +0300

    Write fetcher tests, fix scraper

---
+++ src/background.test.js
@@ -0,0 +1,27 @@
+import { fetchDocument, scrape } from "./scrape";
+import { Dictionary } from './types';
+import { JSDOM } from "jsdom";
+
+const words = {
+    [Dictionary.Turkish]: ["tureng", "text", "word", "binary", "computer"],
+    [Dictionary.French]: ["impossible", "salle", "pan", "batterie", "neuf"],
+    [Dictionary.Spanish]: ["hola", "dulce", "hielo", "elefante", "libro"],
+    [Dictionary.German]: ["nine", "hallo", "deutsch", "mensch", "jein"],
+}
+
+// Tests are run in Node.js, so we must use the jsdom parser.
+const parseDom = (a, b) => new JSDOM(a, b);
+const fetcherFor = dictionary => term => fetchDocument(term, dictionary, parseDom);
+
+const testerFor = (dictionary) => async () => {
+    const fetch = fetcherFor(dictionary);
+    const promise = Promise.all(words[dictionary].map(fetch));
+    await expect(promise).resolves.not.toThrow();
+}
+
+describe("documents", () => {
+    test("are fetched for turkish", testerFor(Dictionary.Turkish));
+    test("are fetched for french", testerFor(Dictionary.French));
+    test("are fetched for spanish", testerFor(Dictionary.Spanish));
+    test("are fetched for german", testerFor(Dictionary.German));
+});
---
+++ src/types.js
@@ -0,0 +1,6 @@
+export const Dictionary = {
+    Turkish: "turkish-english",
+    French: "french-english",
+    Spanish: "spanish-english",
+    German: "german-english",
+}
